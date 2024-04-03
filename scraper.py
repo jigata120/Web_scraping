def01b8619a2250c248103edf7dd3df9207b3556
mihaeldzhigov <pplesho@gmail.com>
2024-04-03 23:19:14 +0300

    responce and food-dict str repr

--- scraper.py
+++ scraper.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 url = 'https://calorienbalans.com/FoodInfo'
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
 def parse_food_data(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -25,3 +26,19 @@ def parse_food_data(html):
             }
 
     return food_data
+
+
+response = requests.get(url, headers=headers)
+html_content = response.content
+
+food_dict = parse_food_data(html_content)
+# print(food_dict)
+def print_dict(dict):
+    for meal,macroses in dict.items():
+        print(f"{meal} - protein /{macroses['protein']}/ ;\
+               fat /{macroses['fat']}/ ; carbohydrate /{macroses['carbohydrate']}/ ;\
+                  calories /{macroses['calories']}/")
+
+# print_dict(food_dict)
+        
+        
