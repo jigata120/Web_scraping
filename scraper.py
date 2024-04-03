29088dc6b664cd1cf862230753f343ff2057e987
mihaeldzhigov <pplesho@gmail.com>
2024-04-03 23:23:46 +0300

    generate html

--- scraper.py
+++ scraper.py
@@ -50,4 +50,25 @@ def optimal_protein(data):
                 
     # print_dict(foods_optimal)
 
-optimal_protein(food_dict)       
+optimal_protein(food_dict)  
+
+
+def generate_html_table(food_dict):
+    html_content = '''<!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <title>Document</title>
+                        </head>
+                        <body><table border="1">\n'''
+
+    html_content += '<tr><th>Food Name</th><th>Protein</th><th>Fat</th><th>Carbohydrate</th><th>Calories</th></tr>\n'
+    js = get_the_js('screpeDisplay.js')
+    for food_name, data in food_dict.items():
+        html_content += f'<tr><td>{food_name}</td><td>{data["protein"]}</td>\
+            <td>{data["fat"]}</td><td>{data["carbohydrate"]}</td><td>{data["calories"]}</td></tr>\n'
+    
+    html_content += f'</table>\n<script>{js}</script>\n</body></html>'
+    
+    return html_content
